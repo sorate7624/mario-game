d51aa60d5863eb89ccb05e32d816dd81db818213
hyejin94 <sorate_@naver.com>
2022-01-29 12:33:17 +0900

    Gravity

--- index.js
+++ index.js
@@ -4,21 +4,45 @@ const c = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+const gravity = 1.5;
 class Player {
   constructor() {
     this.position = {
       x: 100,
       y: 100
     };
-    this.width = 100;
-    this.height = 100;
+    this.velocity = {
+      x: 0,
+      y: 0
+    };
+    this.width = 30;
+    this.height = 30;
   }
 
   draw() {
     c.fillStyle = 'red';
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
+
+  update() {
+    this.position.y += this.velocity.y;
+    this.draw();
+
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+  }
 }
 
 const player = new Player();
-player.draw();
+player.update();
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+}
+
+animate();
