5ddf6b954897b63c959e2c791b4403deaa679ce2
hyejin94 <sorate_@naver.com>
2022-01-30 13:08:44 +0900

    Platforms

--- index.js
+++ index.js
@@ -37,7 +37,24 @@ class Player {
   }
 }
 
+class Platform {
+  constructor() {
+    this.position = {
+      x: 200,
+      y: 100
+    };
+    this.width = 200;
+    this.height = 20;
+  }
+
+  draw() {
+    c.fillStyle = 'blue';
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
 const player = new Player();
+const platform = new Platform();
 const keys = {
   right: {
     pressed: false
@@ -51,6 +68,7 @@ function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
+  platform.draw();
 
   if (keys.right.pressed) {
     player.velocity.x = 5;
@@ -58,6 +76,11 @@ function animate() {
     player.velocity.x = -5;
   } else {
     player.velocity.x = 0;
+  }
+
+  //platform collision detection
+  if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
+    player.velocity.y = 0;
   }
 }
 
